8159c33eaa69ad366a6eb2e700c7199813fc097d
mgibbs189 <mgibbs189@gmail.com>
2019-01-10 16:03:51 -0500

    Tagged 0.5.0

--- facetwp-map-facet.php
+++ facetwp-map-facet.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FacetWP - Map Facet
 Description: Map facet type
-Version: 0.4.5
+Version: 0.5.0
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 GitHub URI: facetwp/facetwp-map-facet
